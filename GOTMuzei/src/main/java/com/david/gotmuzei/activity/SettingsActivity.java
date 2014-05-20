7783987744e7dec52ad0d3c5936702801c3f9135
David Hodge <dhodge.jr229@gmail.com>
2014-05-20 00:55:09 -0400

    spin up settings screen

--- GOTMuzei/src/main/java/com/david/gotmuzei/activity/SettingsActivity.java
+++ GOTMuzei/src/main/java/com/david/gotmuzei/activity/SettingsActivity.java
@@ -1,11 +1,28 @@
 package com.david.gotmuzei.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+
+import com.david.gotmuzei.R;
 
 /**
  * Created by davidhodge on 5/20/14.
  */
 public class SettingsActivity extends Activity {
 
+    CheckBox showTextBox;
+    Spinner updateFreq;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+
+        showTextBox = (CheckBox) findViewById(R.id.text_checkbox);
+        //TODO add update options
+        updateFreq = (Spinner) findViewById(R.id.update_int);
+    }
 
 }
