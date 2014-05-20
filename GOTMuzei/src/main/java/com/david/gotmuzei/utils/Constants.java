39c5f47df5ae8f3a217dc0d944e803f751b470ac
David Hodge <dhodge.jr229@gmail.com>
2014-05-20 00:55:24 -0400

    added some constants

---
+++ GOTMuzei/src/main/java/com/david/gotmuzei/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.david.gotmuzei.utils;
+
+/**
+ * Created by davidhodge on 5/20/14.
+ */
+public class Constants {
+
+    public static final String IMAGE_CREATOR = "Nickorama144";
+    public static final String APP_CREATOR = "David Hodge";
+
+    public static final String IMAGE_CREATOR_LINK = " ";
+    public static final String TWITTER_URL = "https://twitter.com/davidhodge229";
+    public static final String APP_NET_URL = "https://alpha.app.net/david_hodge";
+    public static final String PLUS_URL = "https://plus.google.com/u/0/+DavidHodge229/posts";
+    public static final String EMAIL_URL = "dhodge.jr229@gmail.com";
+    public static final String OTHER_APPS_URL = "https://play.google.com/store/apps/developer?id=Vapr-Ware";
+}
