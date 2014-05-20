a61d35f7d47c38201a58fb164a660f884c214990
David Hodge <dhodge.jr229@gmail.com>
2014-05-20 13:59:57 -0400

    updated info

--- GOTMuzei/src/main/java/com/david/gotmuzei/utils/Constants.java
+++ GOTMuzei/src/main/java/com/david/gotmuzei/utils/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     public static final String IMAGE_CREATOR = "Nickorama144";
     public static final String APP_CREATOR = "David Hodge";
 
-    public static final String APP_CREATOR_LINK = "";
-    public static final String IMAGE_CREATOR_LINK = " ";
+    public static final String APP_CREATOR_LINK = "http://www.reddit.com/user/ThatOfficeMaxGuy/";
+    public static final String IMAGE_CREATOR_LINK = "http://www.reddit.com/user/Nickorama144";
     public static final String TWITTER_URL = "https://twitter.com/davidhodge229";
     public static final String APP_NET_URL = "https://alpha.app.net/david_hodge";
     public static final String PLUS_URL = "https://plus.google.com/u/0/+DavidHodge229/posts";
