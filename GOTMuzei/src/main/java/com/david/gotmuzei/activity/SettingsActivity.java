aad22b0c16898da211758ae99cd3d5d9e9799cce
David Hodge <dhodge.jr229@gmail.com>
2014-05-20 00:04:20 -0400

    project created

---
+++ GOTMuzei/src/main/java/com/david/gotmuzei/activity/SettingsActivity.java
@@ -0,0 +1,11 @@
+package com.david.gotmuzei.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by davidhodge on 5/20/14.
+ */
+public class SettingsActivity extends Activity {
+
+
+}
---
+++ GOTMuzei/src/main/java/com/david/gotmuzei/service/MuzeiService.java
@@ -0,0 +1,9 @@
+package com.david.gotmuzei.service;
+
+/**
+ * Created by davidhodge on 5/20/14.
+ */
+public class MuzeiService extends MuzeiService {
+
+
+}
