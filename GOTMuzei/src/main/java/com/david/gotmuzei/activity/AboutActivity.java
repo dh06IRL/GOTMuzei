82198ca93afa1a5577c8db02ebb583dbd889c77b
David Hodge <dhodge.jr229@gmail.com>
2014-05-20 00:55:17 -0400

    spin up about screen

---
+++ GOTMuzei/src/main/java/com/david/gotmuzei/activity/AboutActivity.java
@@ -0,0 +1,19 @@
+package com.david.gotmuzei.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.david.gotmuzei.R;
+
+/**
+ * Created by davidhodge on 5/20/14.
+ */
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
